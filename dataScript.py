22c9ccba32f84d2f99c78429e7ae5e72149fcda6
Andre Amorim <amorim.pires.andre@gmail.com>
2024-06-06 10:59:04 +0100

    Fixing Bugs
    data base bugs that needed to be resolved, ready to lauch to the good stuff

--- dataScript.py
+++ dataScript.py
@@ -1,29 +1,42 @@
 import sqlite3
 import os
 
+#CONSTANTS
+DATA_BASE = 'gpt.db'
+MAX_ID = "SELECT MAX(vidId) FROM Videos"
+GPT_SCRIPT_FILE = 'gpt.py'
+VIDS_DIR_NAME = 'seriesOfVideos'
+READ_MODE = 'r'
+INSERT_DATA_INTO_TABLE = "INSERT INTO Videos VALUES (?, ?, ?, ?)"
+UNICODE_FORMAT = 'utf-8'
+
 
 #Updating the database
-conn = sqlite3.connect('gpt.db')
+conn = sqlite3.connect(DATA_BASE)
 c = conn.cursor()
+c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(c.fetchall())
 
 # Get the maximum vidId from the Videos table
-c.execute("SELECT MAX(vidId) FROM Videos")
+c.execute(MAX_ID)
 result = c.fetchone()
 vidId = result[0] if result[0] is not None else 0
 
-script_path = os.path.dirname(os.path.abspath('gpt.py'))
-vid_dir_path = os.path.join(script_path, 'seriesOfVideos')
+script_path = os.path.dirname(os.path.abspath(GPT_SCRIPT_FILE))
+vid_dir_path = os.path.join(script_path, VIDS_DIR_NAME)
 files = os.listdir(vid_dir_path)
 
 #Updating the database
 for file in files:
   vidId += 1
   completed_path = os.path.join(vid_dir_path, file)
-  with open(completed_path, 'r', encoding='utf-8') as file_op:
+  with open(completed_path, READ_MODE, encoding = UNICODE_FORMAT) as file_op:
 
     vidName = file_op.readline().strip()
     vidLink = file_op.readline().strip()
     vidText = ''.join(file_op.readlines())
-    c.execute("INSERT INTO Videos VALUES (?, ?, ?, ?)", (vidId, vidName, vidText, vidLink))
+    c.execute(INSERT_DATA_INTO_TABLE, (vidId, vidName, vidText, vidLink))
   conn.commit()
+
+#Removing the same videos
 
--- gpt.py
+++ gpt.py
@@ -6,13 +6,24 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 
-#CREATE THE DATABASE
-connection = sqlite3.connect('gpt.sql')
+#CONSTANTES
+DATA_BASE = 'gpt.db'
+CPU = 'cpu'
+CUDA = 'cuda:0'
+ONLY_VID_TEXT = "SELECT vidText FROM Videos"
+BINARY_FIT_SIZE = 13
 
-with open("file.txt", 'r', encoding='utf-8') as file:
-  text = file.read()
+#CREATE THE DATABASE AND READ DATA FROM THEM
+connection = sqlite3.connect(DATA_BASE)
+cursor = connection.cursor()
+cursor.execute(ONLY_VID_TEXT)
+text = ''
+for vidtext in cursor:
+  text += vidtext[0]
+
+
 #SETUP THE DEVICE
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device(CUDA if torch.cuda.is_available() else CPU)
 
 #GET TOUPLES INDEX
 chars = sorted(list(set(text)))
@@ -27,8 +38,9 @@ for l1, l2 in zip(textL, textL[1:]):
 #BINARY ENCODING n=12  #transform tks in indexes and after we transform in binary(could be more efficient) #s-i-b
 stoi = {s:i for i,s in enumerate(combChars)}
 itos = {i:s for i,s in enumerate(combChars)}
-binaryDict = list(itertools.product([0, 1], repeat=12))
+binaryDict = list(itertools.product([0, 1], repeat= BINARY_FIT_SIZE)) 
 itob = {}
+
 for chs in combChars:
   i = stoi[chs]
   itob[i] = binaryDict[i]
@@ -36,4 +48,8 @@ btoi = {b:i for i,b in itob.items()}
 encode = lambda tks: [itob[stoi[tk]] for tk in tks]
 decode = lambda binN: [itos[btoi[bn]] for bn in binN]
 data = torch.tensor(encode(tks), dtype=torch.long)
+
+print(data[0][:10])
+
+
 
